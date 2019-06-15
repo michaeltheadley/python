eb2761ab42196fe67b8b815b3d5bf70f17e27ccd
Michael Headley <mheadley93@gmail.com>
2019-06-15 13:55:30 -0400

    Update TimesTable.py

--- TimesTable.py
+++ TimesTable.py
@@ -35,17 +35,7 @@ while length < len(column_list):
 
     if marker == len(tableValues):
         break
-
-# this works, which is awesome. let's now work on some logic to "prettyprint"
-# the table so it looks more like a times table:
-
-# for every number in columns, print that number.
-# go to the next line.
-# for every number there was above, print an underscore. This will be the beginning of the table.
-
-# (3) then, for each row, print the FIRST row item, then a bar line, then the next row item, then a bar line.
-#          (must check elements in row_list because we don't want a number at the end NOT contained by a line. )
-# return to (3) until the entire chart is complete.
+        
 
 for i in column_list:
     if i == 1:
@@ -62,10 +52,6 @@ for i in column_list:
         print("--", end= " ")
 
 print("")
-
-# if it's the beginning of a new row, write the value of the row itself (leftmost hand side)
-# ELSE, for i in table values, write each table value followed by the bar line delimiter.
-# must check to see if we're at the end of the row, then START THE LOOP OVER until all rows are complete.
 
 p = 0
 row_string = ""
